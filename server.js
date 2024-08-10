69c375a50d75957c599882bc52195fc0d68a1916
Charan Gowda B M <116708480+Charan-Gowda-BM-2003@users.noreply.github.com>
2024-08-10 23:06:22 +0530

    Frontend

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const userRoutes = require("./routes/user");
+const dotenv = require("dotenv");
+const { connect } = require("mongoose");
+const app = express();
+dotenv.config();
+const PORT = process.env.PORT;
+const DB_URL = process.env.MONGODB_URL;
+
+app.use(express.json());
+app.use(cors());
+app.use("/user", userRoutes);
+app.get("/", (req, res) => {
+  res.status(200).send("Backend has started");
+});
+app.listen(PORT, () => {
+  console.log("listening on PORT: " + PORT);
+});
+
+if (DB_URL) {
+  connect(process.env.MONGODB_URL);
+} else {
+  console.log("DB URL is Invalid");
+}
